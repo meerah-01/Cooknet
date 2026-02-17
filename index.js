562f0135f1e914072c766ce63965db0eda200e57
meerah-01 <ameerahb246@gmail.com>
2026-02-17 09:10:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.mobile-menu');
+const exit = document.querySelector('.exit');
 
 hamburger.addEventListener('click', () => {
-  menu.classList.toggle('active');
+  menu.classList.add('active');
+});
+
+exit.addEventListener('click', () => {
+  menu.classList.remove('active');
 });
