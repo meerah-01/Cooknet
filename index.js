8e4f7a78c505d6f49ea2e37b536006bddfb97b13
meerah-01 <ameerahb246@gmail.com>
2026-02-22 18:22:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.mobile-menu');
-const exit = document.querySelector('.exit');
+const exit = document.querySelector('.exit')
+const remove = document.querySelectorAll('.cart-button');
+const GoTo = document.querySelectorAll('a');
 
 hamburger.addEventListener('click', () => {
   menu.classList.add('active');
@@ -11,4 +13,28 @@ exit.addEventListener('click', () => {
   menu.classList.remove('active');
   hamburger.classList.remove('hide');
 });
+
+remove.forEach(button => {
+  button.addEventListener('click', () => {
+    const productCard = button.closest('.product-info');
+    const added = productCard.querySelector('.added');
+
+    button.remove();
+
+    added.innerHTML = `
+      <i class="fa-solid fa-circle-check" style="color: rgb(53, 132, 107)"></i>
+      <p>Added to Cart</p>
+    `;
+
+    added.classList.add('added-active');
+  });
+});
+
+GoTo.forEach(link => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('hide');
+    menu.classList.remove('active');
+  })
+})
+
 
